dac073a1aec34a94adc5ea75a792be676884c65f
SirZanty <sirzanty@gmail.com>
2022-12-05 11:16:15 -0500

    finish first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,52 @@
 import './App.css'
-import ReactModal from "react-modal";
-import { useState } from "react";
-const App = () => {
-  const [showModal, setModalStatus] = useState(true);
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      backgroundColor: 'rgba(15, 13, 13, 0.1)',
+import { useState, useEffect } from "react";
+import { InputMask } from 'primereact/inputmask';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import axios from 'axios';
 
-    },
-    overlay: {
-      backgroundColor: 'rgba(13, 13, 13, 0.1)',
-    },
+const App = () => {
+  const [showModal, setModalStatus] = useState(false);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const onLoadEffect = () => {
+    setTimeout(() => {
+      setModalStatus(true);
+    }, 1000);
   };
+  useEffect(onLoadEffect, []);
 
   const closeModal = () => {
+    axios.post('https://api.pmcaus.com/api/shopify/callrequest', {
+      firstName: {name},
+      lastName: {phone}
+    })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     setModalStatus(false);
   }
 
+  if (!showModal) {
+    return <div></div>
+  }
   return (
-    <div>
-      <ReactModal
-        isOpen={showModal}
-        contentLabel="The best CPAP and supplies"
-        style={customStyles}
-      >
-        <p>Name/Phone</p>
-        <button onClick={() => closeModal()}>CALL ME!</button>
-      </ReactModal>
+    <div id="main-popup">
+      <div id="header">
+        <img src="https://raw.githubusercontent.com/SirZanty/audio/main/tasksupply.png" />
+        <div id="exit">
+          <button id="exit-button" onClick={() => closeModal()}></button>
+        </div>
+      </div>
+      <div id="main">
+        <InputText placeholder="Full Name" id="name" name="name" onChange={(e) => setName(e.target.value)}  />
+        <InputMask mask="(999)999-9999" id="phone" placeholder="Enter your phone" onChange={(e) => setPhone(e.target.value)}></InputMask>
+        <Button onClick={() => closeModal()} id="button" label="Call me!" />
+      </div>
     </div>
   );
 }
